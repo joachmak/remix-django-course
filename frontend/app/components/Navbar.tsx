eb01162dc46a689ba1e75dae8a27868a47f9be4e
joachmak <joachimmaksim@gmail.com>
2022-03-08 13:08:09 +0100

    Make navbar retain state on refresh

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,20 +1,20 @@
 import { Button, SimpleGrid } from "@mantine/core";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
+import { useLocation } from "react-router";
 import { Link } from "remix";
 
 function CustomMenuButton({
-  id,
-  navState,
+  path,
   children,
 }: {
-  id: number;
-  navState: number;
+  path: string;
   children: ReactNode;
 }) {
-  const isActive = id === navState;
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <Button
-      variant={isActive ? "filled" : "outline"}
+      variant={path === currentPath ? "filled" : "outline"}
       style={{ width: "100%", height: 50, margin: "10px 0", fontSize: 16 }}
     >
       {children}
@@ -22,25 +22,20 @@ function CustomMenuButton({
   );
 }
 
+function CustomLink({ path, text }: { path: string; text: string }) {
+  return (
+    <Link to={path}>
+      <CustomMenuButton path={path}>{text}</CustomMenuButton>
+    </Link>
+  );
+}
+
 export default function Navbar() {
-  const [navState, setNavState] = useState(0);
   return (
     <SimpleGrid style={{ width: "100%" }} cols={3}>
-      <Link to={"/"} onClick={() => setNavState(0)}>
-        <CustomMenuButton id={0} navState={navState}>
-          Hjem
-        </CustomMenuButton>
-      </Link>
-      <Link to={"/hamsters"} onClick={() => setNavState(1)}>
-        <CustomMenuButton id={1} navState={navState}>
-          Hamstere
-        </CustomMenuButton>
-      </Link>
-      <Link to={"/species"} onClick={() => setNavState(2)}>
-        <CustomMenuButton id={2} navState={navState}>
-          Hamster-arter
-        </CustomMenuButton>
-      </Link>
+      <CustomLink path={"/"} text={"Hjem"} />
+      <CustomLink path={"/hamsters"} text={"Hamstere"} />
+      <CustomLink path={"/species"} text={"Hamster-arter"} />
     </SimpleGrid>
   );
 }
