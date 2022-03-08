f3945074a2f8a9b9365073e8ffa14cd4864cda0b
joachmak <joachimmaksim@gmail.com>
2022-03-08 13:08:29 +0100

    Add hamster overview

--- frontend/app/routes/hamsters/index.tsx
+++ frontend/app/routes/hamsters/index.tsx
@@ -1,23 +1,70 @@
+import {
+  Button,
+  Card,
+  Image,
+  SimpleGrid,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import { Link, useLoaderData } from "remix";
+import HeaderText from "~/components/HeaderText";
 import { getHamsters, Hamster } from "~/modules/hamster";
 
 export const loader = async () => {
   return await getHamsters();
 };
 
+function HamsterCard({
+  name,
+  id,
+  image,
+  description,
+}: {
+  name: string;
+  id: number;
+  image: string;
+  description: string;
+}) {
+  const theme = useMantineTheme();
+  return (
+    <Card shadow="sm" padding="lg">
+      <Card.Section>
+        <Image src={image} height={160} alt={name} />
+      </Card.Section>
+      <HeaderText>{name}</HeaderText>
+      <Text size="sm" style={{ color: theme.colors.dark[1], lineHeight: 1.5 }}>
+        {description}
+      </Text>
+
+      <Link to={"/hamsters/" + id} style={{ width: "100%" }}>
+        <Button
+          variant="light"
+          color="blue"
+          fullWidth
+          style={{ marginTop: 14 }}
+        >
+          Se detaljert informasjon
+        </Button>
+      </Link>
+    </Card>
+  );
+}
+
 export default function Posts() {
   const hamsters: Hamster[] = useLoaderData();
   return (
     <>
-      <h1>Hamsters</h1>
-      <ul>
+      <SimpleGrid style={{ width: "100%" }} cols={3}>
         {hamsters.map((hamster) => (
-          <li key={hamster.name}>
-            <Link to={hamster.id.toString()}>{hamster.name}</Link>
-          </li>
+          <HamsterCard
+            key={hamster.id}
+            name={hamster.name}
+            id={hamster.id}
+            image={hamster.image}
+            description={hamster.description}
+          />
         ))}
-      </ul>
-      <Link to={"/"}>Home</Link>
+      </SimpleGrid>
     </>
   );
 }
